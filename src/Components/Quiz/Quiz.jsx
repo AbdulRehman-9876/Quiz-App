3b314b74a92b47aeaf9d2a03cef41790ddc01b7d
Abdul Rehman <abdulrehman9876@gmail.com>
2024-10-25 00:46:46 +0500

    Dynamic Functionality Added

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,22 +1,59 @@
-import React from 'react'
-import './Quiz.css'
+import React, { useState, useEffect, useRef } from 'react';
+import './Quiz.css';
+import { data } from '../../assets/data';
 
 const Quiz = () => {
+  let [index, setIndex] = useState(0); //index of question
+  let [question_, setQuestion] = useState(data[index]); //question data
+  let [lock,setLock] = useState(false); //multi select disabled
+
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let optionArray = [Option1,Option2,Option3,Option4]; //correct option identified
+
+  const checkAns = (e,ans) => {
+    if(lock === false){
+
+        if(question_.ans === ans){
+            e.target.classList.add("correct");
+        } else{
+            e.target.classList.add("wrong");
+            optionArray[question_.ans - 1].current.classList("correct");
+        }
+
+        setLock(true); 
+    }
+  }
+
+  useEffect(() => {
+    setQuestion(data[index]);
+  }, [index]); 
+
+  const handleNext = () => {
+    if (index < data.length - 1) {
+      setIndex(index + 1);
+    }
+  };
+
   return (
     <div className='container'>
-        <h1>Quiz App</h1>
-        <hr/>
-        <h2>Which device is required for the internet connection</h2>
-        <ul>
-            <li>Modem</li>
-            <li>Router</li>
-            <li>LAN Cable</li>
-            <li>Pen Drive</li>
-        </ul>
-        <button>Next</button>
-        <div className="index">1 of 5 questions</div>
-    </div>
-  )
-}
+      <h1>Quiz App</h1>
+      <hr />
+      <h2>{index + 1}. {question_.question}</h2>
+      <ul>
+        <li ref={Option1} onClick={(e) => {checkAns(e,1)}}>{question_.option1}</li>
+        <li ref={Option2} onClick={(e) => {checkAns(e,2)}}>{question_.option2}</li>
+        <li ref={Option3} onClick={(e) => {checkAns(e,3)}}>{question_.option3}</li>
+        <li ref={Option4} onClick={(e) => {checkAns(e,4)}}>{question_.option4}</li>
+      </ul>
 
-export default Quiz
+      <button onClick={handleNext}>Next</button>
+      <div className="index">{index + 1} of {data.length} questions</div>
+    </div>
+  );
+};
+
+export default Quiz;
